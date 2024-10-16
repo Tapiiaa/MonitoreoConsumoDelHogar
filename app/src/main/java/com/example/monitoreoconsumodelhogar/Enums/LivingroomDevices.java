f9bed62b5e111a0cdaf1d5b06abeda587050ffd8
Patrik <psirb@myuax.com>
2024-10-16 23:17:02 +0200

    Enums

---
+++ app/src/main/java/com/example/monitoreoconsumodelhogar/Enums/LivingroomDevices.java
@@ -0,0 +1,25 @@
+package com.example.monitoreoconsumodelhogar.Enums;
+
+public enum LivingroomDevices {
+
+    TELEVISION(1.0),
+    LUZ(1.0),
+    VENTILADOR(0.7),
+    CARGADOR_MOVIL(0.05),
+    AIRE_ACONDICIONADO(3.0),
+    CARGADOR_ASPIRADORA(0.2),
+    ORDENADOR(0.5),
+    CARGADOR_PORTATIL(0.1),
+    LAMPARA_DE_PIE(0.6),
+    ROUTER(0.05);
+
+    private final double kwh;
+
+    LivingroomDevices(double kwh) {
+        this.kwh = kwh;
+    }
+
+    public double getKwh() {
+        return kwh;
+    }
+}
