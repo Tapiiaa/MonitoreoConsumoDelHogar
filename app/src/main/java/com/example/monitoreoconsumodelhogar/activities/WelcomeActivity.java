87bf0b378ffe3fe694f11272fb75247102f55636
Patrik <psirb@myuax.com>
2024-10-17 01:39:19 +0200

    Nos aseguramos de que los cuadros de texto del login no se pueden dejar en blanco.

--- app/src/main/java/com/example/monitoreoconsumodelhogar/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/monitoreoconsumodelhogar/activities/WelcomeActivity.java
@@ -1,10 +1,12 @@
 package com.example.monitoreoconsumodelhogar.activities;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.monitoreoconsumodelhogar.R;
 
 public class WelcomeActivity extends AppCompatActivity {
