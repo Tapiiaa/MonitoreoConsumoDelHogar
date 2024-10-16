a53690a33f3bf8521f687256e986583be81141b0
Tapiiaa <ptapilob@myuax.com>
2024-10-16 11:08:29 +0200

    Enlaza welcome con main

---
+++ app/src/main/java/com/example/monitoreoconsumodelhogar/activities/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.monitoreoconsumodelhogar.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.monitoreoconsumodelhogar.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción al presionar el botón
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
